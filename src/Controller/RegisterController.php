56f12ef37512fd89e00403883276a721bc1667f3
Assane Thione Ba <assanetb@gmail.com>
2020-04-13 16:33:26 +0000

    Add upload profile image to user registration

--- src/Controller/RegisterController.php
+++ src/Controller/RegisterController.php
@@ -4,6 +4,7 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\UserType;
+use App\Service\UploadFile;
 use Doctrine\Common\Persistence\ObjectManager;
 use function dump;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
@@ -11,12 +12,12 @@ use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
-class SecurityController extends AbstractController
+class RegisterController extends AbstractController
 {
     /**
-     * @Route("/register1", name="register")
+     * @Route("/register", name="register")
      */
-    public function registration(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
+    public function registration(Request $request, ObjectManager $manager, UploadFile $uploadFile, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
 
@@ -28,32 +29,28 @@ class SecurityController extends AbstractController
             $user->setRole('ROLE_USER');
             $user->setCreatedAt(new \DateTime());
 
+            if($form['profileImage']->getData())
+            {
+                $profileImagePath = $uploadFile->upload($form['profileImage']->getData());
+                $user->setProfileImage($profileImagePath);
+            }
+            else {
+                $user->setProfileImage('default-profile.png');
+            }
+
             $hashedPassword = $encoder->encodePassword($user, $user->getPassword());
             $user->setPassword($hashedPassword);
 
+            //dump($user);
             $manager->persist($user);
             $manager->flush();
 
-            return $this->redirectToRoute('login');
+            return $this->redirectToRoute('app_login');
         }
 
         return $this->render('security/registration.html.twig', [
             'form' => $form->createView(),
         ]);
     }
-
-    /**
-     * @Route("/login1", name="login")
-     */
-    public function login()
-    {
-
-        return $this->render('security/login1.html.twig');
-    }
-
-    /**
-     * @Route("/logout1", name="logout")
-     */
-    public function logout() {}
 
 }
